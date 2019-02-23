34437f3eeb3dc157f8d7289857656cf7ac1005f0
andyd <tungmd2@illinois.edu>
2019-02-23 14:32:56 -0600

    import ocr modules

--- Credibility.py
+++ Credibility.py
@@ -1,12 +1,13 @@
+from PIL import Image
 import cv2 as cv
+import pytesseract as ocr
 def image_verification(image):
 
     # read in the image
     image = cv.imread('filename.jpg')
-    gray_image = gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
 
     # applying Gaussian blur to the image to remove noise
-    gray_image = cv.cvtColor(gray_image, cv.COLOR_RGB2GRAY)
+    gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     blur = cv.GaussianBlur(gray_image, (5, 5), 0)
 
     # display the image that has had noise removed
