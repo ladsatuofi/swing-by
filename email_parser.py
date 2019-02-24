89413baaeddd29414708b07f46b7d247b753e2e5
andyd <tungmd2@illinois.edu>
2019-02-24 05:44:41 -0600

    make post request to server

--- email_parser.py
+++ email_parser.py
@@ -1,6 +1,9 @@
+import urllib.request
+
+import os.path
+import json
 import datefinder
 import location_extractor as loc
-import urllib.request
 from bs4 import BeautifulSoup
 
 
@@ -22,7 +25,8 @@ def date_extraction(copy_of_text):
             start_date = date
         if date > end_date:
             end_date = date
-    return (start_date.__format__('%c'), end_date.__format__('%c'))
+    return (start_date, end_date)
+
 
 def location_extraction(text):
     matches = loc.extract_locations_from_text(text)
@@ -32,7 +36,6 @@ def location_extraction(text):
 
 
 def retrieve_buildings():
-    #look at nitish's code, only run once, push to json file, read for every other access
     response = urllib.request.urlopen('http://fs.illinois.edu/about-us/building-list-by-building-number')
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
@@ -44,9 +47,11 @@ def retrieve_buildings():
                 building_name = ''
                 for element in link.find_all('td'):
                     count += 1
-                    if count == 2 and element.string is not None and element.string.strip() != '' and type(element.find('p')) == type(element.find('hfjhefb')):
+                    if count == 2 and element.string is not None and element.string.strip() != '' and type(
+                            element.find('p')) == type(element.find('hfjhefb')):
                         building_name = element.string.strip()
-                    elif count == 3 and element.string is not None and element.string.strip() != '' and type(element.find('p')) == type(element.find('hfjhefb')):
+                    elif count == 3 and element.string is not None and element.string.strip() != '' and type(
+                            element.find('p')) == type(element.find('hfjhefb')):
                         buildings[building_name] = element.string.strip()
     # because there's random <p> tags for these buildings ? ? ?
     del buildings['']
@@ -61,3 +66,14 @@ def match_location_to_building(location):
     for building_name in retrieve_buildings().keys():
         if building_name.lower().__contains__(location.lower()):
             return (building_name, retrieve_buildings()[building_name])
+
+
+if __name__ == '__main__':
+    if os.path.exists("data.json"):
+        with open("data.json") as f:
+            categories = json.load(f)
+        # otherwise, create the file by scraping the UIUC building list
+    else:
+        buildings = retrieve_buildings()
+        with open("data.json", "w") as f:
+            json.dump(buildings, f)
