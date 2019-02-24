df8e985ec39ddb498c70cd31b46f3e11348f6e23
andyd <tungmd2@illinois.edu>
2019-02-24 03:19:12 -0600

    get correct info from email

--- email_parser.py
+++ email_parser.py
@@ -1,21 +1,63 @@
-from PIL import Image
-import cv2 as cv
-from pytesseract import image_to_string
 import datefinder
 import location_extractor as loc
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def filter_by_year(date):
+    if (date.year != 2019):
+        return False
+    else:
+        return True
 
 
 # parse dates
 def date_extraction(copy_of_text):
     matches = datefinder.find_dates(copy_of_text)
-    #throw out extraneous dates (non 2019)
-    #throw out dates lasting over a week
-    for match in matches:
-        print(match)
-
+    matches = list(filter(filter_by_year, matches))
+    start_date = matches[0]
+    end_date = matches[0]
+    for date in matches:
+        if date < start_date:
+            start_date = date
+        if date > end_date:
+            end_date = date
+    return (start_date.__format__('%c'), end_date.__format__('%c'))
 
 def location_extraction(text):
     matches = loc.extract_locations_from_text(text)
-    #search for most viable location
     for match in matches:
-        print(match)
+        if match_location_to_building(match) is not None:
+            return match_location_to_building(match)
+
+
+def retrieve_buildings():
+    #look at nitish's code, only run once, push to json file, read for every other access
+    response = urllib.request.urlopen('http://fs.illinois.edu/about-us/building-list-by-building-number')
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    buildings = dict()
+    for link in soup.find_all('tr'):
+        for key in link.attrs:
+            if key == 'height':
+                count = 0
+                building_name = ''
+                for element in link.find_all('td'):
+                    count += 1
+                    if count == 2 and element.string is not None and element.string.strip() != '' and type(element.find('p')) == type(element.find('hfjhefb')):
+                        building_name = element.string.strip()
+                    elif count == 3 and element.string is not None and element.string.strip() != '' and type(element.find('p')) == type(element.find('hfjhefb')):
+                        buildings[building_name] = element.string.strip()
+    # because there's random <p> tags for these buildings ? ? ?
+    del buildings['']
+    buildings['CERL – Plant/Soil Response Facility'] = '2902 Newmark Drive'
+    buildings['Golf Turf Care Center'] = '2010 South Wright Street'
+    buildings['Grounds Shed'] = 'Adjacent to 501 E. Daniels St.'
+    buildings['Siebel Center for Design'] = '1208 S. Fourth'
+    return buildings
+
+
+def match_location_to_building(location):
+    for building_name in retrieve_buildings().keys():
+        if building_name.lower().__contains__(location.lower()):
+            return (building_name, retrieve_buildings()[building_name])
