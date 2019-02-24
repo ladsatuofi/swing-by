b7af43b8b3b7fb321be65c564818cb9b6b70e117
Steven Li <levensti@gmail.com>
2019-02-24 05:33:35 -0600

    Passing in event object parameters to test app schema.

--- action.py
+++ action.py
@@ -1,14 +1,2 @@
 class Action:
-    def __init__(self, event, num_upvotes, num_downvotes):
-        self.event = event
-        self.num_upvotes = num_upvotes
-        self.num_downvotes = num_downvotes
-    def upvote(event):
-        event.num_upvotes += 1
-    def downvote(event):
-        event.num_downvotes += 1
-    def post_event(self, description, date, time, location):
-        self.description = description
-        self.date = date
-        self.time = time
-        self.location = location
+    def __init__(self):
