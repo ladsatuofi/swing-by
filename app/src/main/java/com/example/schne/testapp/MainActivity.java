8a397966cd511d9fcf09c8339b260feedacdb5c0
Sami Alqadi <salqadi3@illinois.edu>
2019-02-23 22:49:33 -0600

    Updated Login Activity with proper color scheme and added vector photo

--- app/src/main/java/com/example/schne/testapp/MainActivity.java
+++ app/src/main/java/com/example/schne/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.schne.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signIn = (Button) findViewById(R.id.signInButton);
     }
 
 
