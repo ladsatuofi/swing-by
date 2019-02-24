21a24513c0acbfbe9e1cb57d347319be5138f001
Sami Alqadi <salqadi3@illinois.edu>
2019-02-24 02:22:31 -0600

    Added Event objects

---
+++ app/src/main/java/com/example/schne/testapp/Event.java
@@ -0,0 +1,12 @@
+package com.example.schne.testapp;
+
+import java.util.List;
+
+public class Event {
+    private String name;
+    private String description;
+    private String location;
+    private String time;
+    private List<String> tags;
+    private String uuid;
+}
--- app/src/main/java/com/example/schne/testapp/EventFeedActivity.java
+++ app/src/main/java/com/example/schne/testapp/EventFeedActivity.java
@@ -1,4 +1,5 @@
 package com.example.schne.testapp;
 
 public class EventFeedActivity {
+
 }
